953a832c51512ee3e1998950c7ffdc04bed80d2c
Greg Mohr <gregmohr2000@yahoo.com>
2023-01-27 20:30:34 -0800

    change landing content switching to useState

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,21 +4,21 @@ import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
 import ForgotPassword from "../components/ForgotPassword";
 
+function Content() {
+  const [content, setContent] = useState('signin');
+
+  if(content === 'signin') return <SignIn setContent={setContent} />;
+  if(content === 'signup') return <SignUp setContent={setContent} />;
+  if(content === 'forgot') return <ForgotPassword setContent={setContent} />;
+}
+
 function LandingPage() {
-  let rightPanel = useState(<SignIn />);
-
-  if (window.location.pathname.match("/signup")) {
-    rightPanel = <SignUp />;
-  } else if (window.location.pathname.match("/forgot")) {
-    rightPanel = <ForgotPassword />;
-  }
-
   return (
     <div className="flex flex-wrap h-screen w-screen justify-center bg-white">
       {/* Banner Panel*/}
       <Banner />
       {/* Sign In / Sign Up / Forgot Password Panel */}
-      {rightPanel}
+      <Content />
     </div>
   );
 }
