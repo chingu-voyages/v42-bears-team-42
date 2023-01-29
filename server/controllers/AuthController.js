3ff15eada70959b8a95ae0fbe1c6a7a46a15331b
Greg Mohr <gregmohr2000@yahoo.com>
2023-01-28 21:34:32 -0800

    complete forgot password and reset password

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,5 +1,6 @@
 import Employee from '../models/EmployeeModel.js';
-import EmployeeController from './EmployeeController.js';
+import sendEmail from '../utils/sendEmail.js';
+import crypto from 'crypto';
 
 // TODO: Confirm res status codes
 const signUp = async (req, res, next) => {
@@ -48,11 +49,66 @@ const signIn = async (req, res, next) => {
 }
 
 const forgotPassword = async (req, res, next) => {
-  res.send(`auth.forgotPassword`);
+  const { email } = req.body;
+
+  try {
+    const employee = await Employee.findOne({ email });
+    if(!employee) return res.status(404).json({ success: false, message: 'Email address not found' });
+
+    const resetToken = employee.generateResetPasswordToken();
+
+    await employee.save();
+
+    // Create email
+    const resetURL = `http://localhost:3000/resetpassword/${resetToken}`;
+    const message = `
+      <h1>Password reset requested</h1>
+      <p>A password reset request was submitted, for this email address, to SAM.
+      We have found your email address on file and you may reset your password by following the link below</p>
+      <a href=${resetURL} clicktracking=off>${resetURL}</a>
+      <p>If you didn't make this request, feel free to ignore it.</p>
+    `;
+    try {
+      await sendEmail({
+        to: employee.email,
+        subject: 'Password Reset',
+        text: message
+      });
+
+      res.status(200).json({ success: true, data: 'Password Reset email sent' });
+    } catch (error) {
+      employee.resetPasswordToken = undefined;
+      employee.resetPasswordExpire = undefined;
+      await employee.save();
+
+      return res.status(500).json({ success: false, message: 'Email could not be sent' });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 }
 
 const resetPassword = async (req, res, next) => {
-  res.send(`auth.resetPassword`);
+  const resetPasswordToken = crypto.createHash('sha256').update(req.params.resetToken).digest('hex');
+
+  try {
+    const employee = await Employee.findOne({
+      resetPasswordToken,
+      resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if(!employee) return res.status(400).json({ success: false, error: "Invalid Reset Token" });
+
+    employee.password = req.body.password;
+    employee.resetPasswordToken = undefined;
+    employee.resetPasswordExpire = undefined;
+
+    await employee.save();
+
+    res.status(201).json({ success: true, message: 'Password reset successful' });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 }
 
 export default { signUp, signIn, forgotPassword, resetPassword };
