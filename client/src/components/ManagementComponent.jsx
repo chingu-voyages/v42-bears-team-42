37739627025f7b5401e8b3f888de3621f682b8a1
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2023-02-01 12:32:18 -0800

    state getting passed down to dashboard component

---
+++ client/src/components/ManagementComponent.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ManagementComponent({ setPermissions }) {
+  return <div>Management Component</div>;
+}
--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
-import Content from "../components/Content";
+import ManagementComponent from "../components/ManagementComponent";
+import EmployeeComponent from "../components/EmployeeComponent";
 
 export default function DashboardPage() {
+  function DashboardComponent() {
+    const [permissions, setPermissions] = useState("manager");
+
+    if (permissions === "manager")
+      return <ManagementComponent setPermissions={setPermissions} />;
+    if (permissions === "employee")
+      return <EmployeeComponent setPermissions={setPermissions} />;
+  }
+
   return (
     <>
       <Header />
-      <Content />
+      <DashboardComponent />
     </>
   );
 }
