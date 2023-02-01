0b2902999c576cf63e8e14e455d2081200cc5d55
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2023-02-01 14:14:20 -0800

    commit before pulling changes

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,60 +1,74 @@
-import Employee from '../models/EmployeeModel.js';
-import sendEmail from '../utils/sendEmail.js';
-import crypto from 'crypto';
+import Employee from "../models/EmployeeModel.js";
+import sendEmail from "../utils/sendEmail.js";
+import crypto from "crypto";
 
 // TODO: Confirm res status codes
 const signUp = async (req, res, next) => {
   const { firstName, lastName, email, password } = req.body;
   const dupEmail = await Employee.findOne({ email });
-  console.log('signUp')
-  if(dupEmail) {
-    return res.status(400).json({ 
+  if (dupEmail) {
+    return res.status(400).json({
       success: false,
-      error: "Email address already in use."
+      error: "Email address already in use or waiting for account verification",
     });
   }
-  
-  const employee = await Employee.create({ 
+
+  const employee = await Employee.create({
     firstName,
     lastName,
     email,
     password,
-    permissions: 'manager'
+    permissions: "manager",
   });
-  if(!employee) return res.status(500).json({ success: false, error: error.message });
-  
+  if (!employee)
+    return res.status(500).json({ success: false, error: error.message });
+
   // Email address verification: clicking link/button in email should auto-sign in user
-  
+
   res.status(201).json({ success: true, employee });
-}
+};
 
 const signIn = async (req, res, next) => {
   const { email, password } = req.body;
 
-  if(!email || !password) {
-    return res.status(400).json({ success: false, error: "Please provide valid email and password" });
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      error: "Please provide valid email and password",
+    });
   }
 
   try {
     const employee = await Employee.findOne({ email }).select("+password");
-    if(!employee) return res.status(404).json({ success: false, error: "Invalid credentials" });
+    if (!employee)
+      return res
+        .status(404)
+        .json({ success: false, error: "Invalid credentials" });
 
     const isMatch = await employee.matchPasswords(password);
-    if(!isMatch) return res.status(404).json({ success: false, error: "Invalid credentials" });
+    if (!isMatch)
+      return res
+        .status(404)
+        .json({ success: false, error: "Invalid credentials" });
 
     const token = employee.generateAuthToken();
-    res.status(200).json({ success: true, permissions: employee.permissions, token });
+    res
+      .status(200)
+      .json({ success: true, permissions: employee.permissions, token });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
   }
-}
+};
 
 const forgotPassword = async (req, res, next) => {
   const { email } = req.body;
 
   try {
     const employee = await Employee.findOne({ email });
-    if(!employee) return res.status(404).json({ success: false, message: 'Email address not found' });
+    if (!employee)
+      return res
+        .status(404)
+        .json({ success: false, message: "Email address not found" });
 
     const resetToken = employee.generateResetPasswordToken();
 
@@ -72,33 +86,43 @@ const forgotPassword = async (req, res, next) => {
     try {
       await sendEmail({
         to: employee.email,
-        subject: 'Password Reset',
-        text: message
+        subject: "Password Reset",
+        text: message,
       });
 
-      res.status(200).json({ success: true, data: 'Password Reset email sent' });
+      res
+        .status(200)
+        .json({ success: true, data: "Password Reset email sent" });
     } catch (error) {
       employee.resetPasswordToken = undefined;
       employee.resetPasswordExpire = undefined;
       await employee.save();
 
-      return res.status(500).json({ success: false, message: 'Email could not be sent' });
+      return res
+        .status(500)
+        .json({ success: false, message: "Email could not be sent" });
     }
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
-}
+};
 
 const resetPassword = async (req, res, next) => {
-  const resetPasswordToken = crypto.createHash('sha256').update(req.params.resetToken).digest('hex');
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resetToken)
+    .digest("hex");
 
   try {
     const employee = await Employee.findOne({
       resetPasswordToken,
-      resetPasswordExpire: { $gt: Date.now() }
-    })
+      resetPasswordExpire: { $gt: Date.now() },
+    });
 
-    if(!employee) return res.status(400).json({ success: false, error: "Invalid Reset Token" });
+    if (!employee)
+      return res
+        .status(400)
+        .json({ success: false, error: "Invalid Reset Token" });
 
     employee.password = req.body.password;
     employee.resetPasswordToken = undefined;
@@ -106,10 +130,12 @@ const resetPassword = async (req, res, next) => {
 
     await employee.save();
 
-    res.status(201).json({ success: true, message: 'Password reset successful' });
+    res
+      .status(201)
+      .json({ success: true, message: "Password reset successful" });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
-}
+};
 
 export { signUp, signIn, forgotPassword, resetPassword };
