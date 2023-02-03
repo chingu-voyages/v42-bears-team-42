41ca9a19b6765db37110a85ed6fb2e8f83212b13
Greg Mohr <gregmohr2000@yahoo.com>
2023-02-03 11:16:50 -0800

    add resetpassword, still need test

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import LandingPage from "./pages/LandingPage";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
+import ResetPassword from "./components/ResetPassword";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route element={ <ProtectedRoutes /> } >
           <Route path="/dashboard" element={ <DashboardPage /> } />
+          <Route path="/resetpassword/:resetToken" element={ <ResetPassword resetToken='resetToken' /> }></Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/components/ResetPassword.jsx
+++ client/src/components/ResetPassword.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Banner from './Banner';
+import logo from '../logo-black.png'
 
-function ResetPassword() {
+function ResetPassword(resetToken) {
   const [newPassword, setNewPassword] = useState('');
   const [error, setError] = useState();
 
@@ -16,10 +18,10 @@ function ResetPassword() {
 
   const resetPasswordHandler = async (e) => {
     e.preventDefault();
+    console.log('resetToken', resetToken)
+    if( !newPassword ) return displayError('Please enter a new password');
 
-    if( !newPassword ) return displayError('Please enter a new passwords');
-
-    return await fetch('http://localhost:3000/api/auth/signin', {
+    return await fetch(`https://samapp-production.up.railway.app/api/auth/resetpassword/${resetToken}`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({newPassword})
@@ -39,10 +41,32 @@ function ResetPassword() {
   }
 
   return ( 
-    <div>
-      <div>{error && <span className="text-red-900">{error}</span>}</div>
-      <div onChange={e => setNewPassword(e.target.value)}></div>
-      <div onClick={ resetPasswordHandler }></div>
+    <div className="flex flex-wrap h-screen w-screen justify-center bg-white">
+      <Banner />
+      <div className="flex flex-col w-1/4 content-center px-2">
+        <img className="mx-auto h-24 mb-16 mt-8" src={logo} alt="app name and logo" />
+        <div className="w-full content-center justify-center border-0">
+          <h1 className="text-3xl font-semibold text-center">WELCOME</h1>
+          <div className='text-gray-400 text-center'>
+            {error && <span className="text-red-900">{error}</span>}
+            {!error && <span className="">Please enter your new password</span>}
+          </div>
+          <div className="mt-4 mb-3">
+            <label htmlFor="" className="mb-2 block text-xs font-semibold">New Password</label>
+            <input
+              type="password"
+              placeholder="New Password"
+              onChange={e => setNewPassword(e.target.value)}
+              className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+            />
+          </div>
+          <div className="mb-3">
+            <button onClick={ resetPasswordHandler } className="mb-1.5 block w-full text-center text-white bg-purple-700 hover:bg-purple-900 px-2 py-1.5 rounded-md">
+              Reset Password
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
