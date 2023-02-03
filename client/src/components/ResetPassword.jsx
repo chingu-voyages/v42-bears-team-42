dbf93d7fdfc225d50598c981ff02669ac670aefe
Greg Mohr <gregmohr2000@yahoo.com>
2023-02-03 15:15:19 -0800

    fix resetPassword fetch string

--- client/src/components/ResetPassword.jsx
+++ client/src/components/ResetPassword.jsx
@@ -24,7 +24,7 @@ function ResetPassword({ match }) {
     if( !newPassword ) return displayError('Please enter a new passwords');
 
     //https://samapp-production.up.railway.app
-    return await fetch('https://samapp-production.up.railway.app/api/auth/resetpassword/${resetToken}', {
+    return await fetch(`https://samapp-production.up.railway.app/api/auth/resetpassword/${resetToken}`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({newPassword})
