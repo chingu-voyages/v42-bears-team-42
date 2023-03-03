1d9077b7004792944cc5ae415254d7c60455a118
Greg Mohr <gregmohr2000@yahoo.com>
2023-03-03 12:57:13 -0800

    add Save and Send buttons

--- client/src/components/Scheduler/Calender.js
+++ client/src/components/Scheduler/Calender.js
@@ -132,16 +132,23 @@ const Calender = () => {
   return (
     <>
       <div className='flex justify-between w-full p-1 border-2 border-purple-700 border-b-0 rounded-t-lg text-2xl bg-black'>
-        {/* < Button */}
-        <button className="px-1 mx-1 border-solid border-2 border-purple-700 rounded-lg text-purple-700" onClick={decrementWeek}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-          </svg>
-        </button>
+        <div className='flex justify-between'>
+          {/* < Button */}
+          <button className="px-1 mx-1 border-solid border-2 border-purple-700 rounded-lg text-purple-700" onClick={decrementWeek}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+            </svg>
+          </button>
+          {/* Save Button */}
+          <button className="text-xs font-semibold border-2 border-purple-700 rounded-full ml-1 bg-purple-400 py-0 px-4"
+                  onClick={()=> console.log('saving')}>
+            Save
+          </button>
+        </div>
         {/* Month Control */}
         <div className="flex flex-row">
           { monthsDisplayed[0] !== monthsDisplayed[1] && <span className="text-purple-400">{monthsDisplayed[0] + ' / '}</span> }
-          <select className="flex-1 flex-shrink bg-black cursor-pointer text-purple-700 text-right"
+          <select className="flex-1 flex-shrink w-fit bg-black cursor-pointer text-purple-700 text-right"
                   onChange={(e) => monthSelect(e.target.value)}
                   value={monthsDisplayed[1]}
                   name="months"
@@ -150,12 +157,19 @@ const Calender = () => {
           </select>
           <div className="text-purple-700 ml-4">{year}</div>
         </div>
-        {/* > Button */}
-        <button className="px-1 mx-1 border-solid border-2 border-purple-700 rounded-lg text-purple-700" onClick={incrementWeek}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </button>
+        <div className="flex justify-between">
+          {/* Send Button */}
+          <button className="text-xs font-semibold border-2 border-purple-700 rounded-full mr-1 bg-purple-400 py-0 px-4"
+                  onClick={()=> console.log('saving')}>
+            Send
+          </button>
+          {/* > Button */}
+          <button className="px-1 mx-1 border-solid border-2 border-purple-700 rounded-lg text-purple-700" onClick={incrementWeek}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+          </button>
+        </div>
       </div>
       <div className="flex justify-end w-full border-purple-700 border-x-2 bg-white">
         <div className="w-9/12">
@@ -178,4 +192,6 @@ const Calender = () => {
   )
 }
 
+//px-1 mx-1 border-solid border-2 border-purple-700 rounded-lg text-purple-700
+//px-1 mx-1 border-solid border-2 border-purple-700 rounded-lg text-purple-700
 export default Calender;
